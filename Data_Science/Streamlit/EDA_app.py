f72e75cd208c09004beae0a1e612987b3e5dd503
NewHarmony <emgleniese15@fudan.edu.cn>
2021-02-13 06:24:03 -0500

    Finished EDA app

--- Data_Science/Streamlit/EDA_app.py
+++ Data_Science/Streamlit/EDA_app.py
@@ -13,7 +13,39 @@ Credit: This App is based on the tutorial of [Chanin Nantasenamat](https://www.y
 
 # Upload CSV data
 with st.sidebar.header('1. Upload your data in CSV file format'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file")
-    st.sidebar.markdown("""
-    [Example CSV input file](insert_link)
-    """)
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type =["csv"])
+    # st.sidebar.markdown("""
+    # [Example CSV input file](https://raw.githubusercontent.com/NewHarmony/100_Days_of_Code/data/master/Data_Science/Streamlit/percent_bachelors_degrees_women_usa.csv)
+    # """)
+
+#Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('--')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Waiting for CSV file to be uploaded.')
+    if st.button('Click to use Example Dataset'):
+        #Example data
+        @st.cache
+        def load_data():
+            csv = pd.read_csv("percent_bachelors_degrees_women_usa.csv")
+            return csv
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write("**Percentage Of Bachelors Degrees For Women In The USA **")
+        st.write("Pandas example dataset")
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas_Profiling_Report**')
+        st_profile_report(pr)
+
