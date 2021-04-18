d7436245719e789e937db3fa6e586daf8623c411
NewHarmony <emgleniese15@fudan.edu.cn>
2021-04-17 23:51:52 -0400

    Initial commit Predicting Stock Prices LSTM

---
+++ Data_Science/TimeSeries/tensorflow_env/Predicting_Stock _Prices_LSTM.py
@@ -0,0 +1,10 @@
+import numpy as np
+import mathplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+
+
+from sklearn.preprocessing import MinMaxScaler
+form tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
