0352041be4cb0c31c05ccbaadffd3aa6eb98bcea
NewHarmony <emgleniese15@fudan.edu.cn>
2021-04-20 23:53:31 -0400

    Adding to Predicting_Stock_Prices_LSTM

--- Data_Science/TimeSeries/tensorflow_env/Predicting_Stock _Prices_LSTM.py
+++ Data_Science/TimeSeries/tensorflow_env/Predicting_Stock _Prices_LSTM.py
@@ -1,5 +1,5 @@
 import numpy as np
-import mathplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader as web
 import datetime as dt
@@ -8,3 +8,27 @@ import datetime as dt
 from sklearn.preprocessing import MinMaxScaler
 form tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+# Load Data
+company = "GOOG"
+
+start = dt.datetime(2012,1,1)
+end = dt.datetime(2020,1,1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+# Prepare Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 600
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
